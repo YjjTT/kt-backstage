89c8180c6e83160a25f3757c4753078f1352536b
YjjTT <yjjtt@tempot.com>
2019-03-18 15:52:53 +0800

    Updates

---
+++ precache-manifest.6a568baf3630ddfdb6ce906f7e9ff870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ceb422a1e186152cf8c",
+    "url": "/kt-backstage/static/css/main.498b62fb.chunk.css"
+  },
+  {
+    "revision": "9ceb422a1e186152cf8c",
+    "url": "/kt-backstage/static/js/main.7e0031ba.chunk.js"
+  },
+  {
+    "revision": "4d8acbe7d8c72b9c7519",
+    "url": "/kt-backstage/static/js/runtime~main.217811ff.js"
+  },
+  {
+    "revision": "30bbd8bbb90338069c27",
+    "url": "/kt-backstage/static/css/2.8b7e4b7b.chunk.css"
+  },
+  {
+    "revision": "30bbd8bbb90338069c27",
+    "url": "/kt-backstage/static/js/2.40464fa6.chunk.js"
+  },
+  {
+    "revision": "aab5b42eb64ae224abf82e12bdc2cd90",
+    "url": "/kt-backstage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d28815d21546b294a9a0238138e89bf7.js"
+  "/kt-backstage/precache-manifest.6a568baf3630ddfdb6ce906f7e9ff870.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kt-backstage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
